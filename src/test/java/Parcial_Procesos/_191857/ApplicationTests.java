57b202664c1e7e659eccdad1bcf6ebe010a11bd6
PCUniversidad <ngoodwinb_p672i@tigpe.com>
2022-10-06 18:43:14 -0500

    Parcial procesos

---
+++ src/test/java/Parcial_Procesos/_191857/ApplicationTests.java
@@ -0,0 +1,13 @@
+package Parcial_Procesos._191857;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
